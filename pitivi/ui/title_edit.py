5c040b53061885e037d825644583e5a6e281f4b9
Dafydd Harries <daf@rhydd.org>
2010-03-27 16:28:49 -0400

    title dialog: accept properties in constructor
    Also, copy our properties to dialog widgets before running the dialog.

--- pitivi/ui/title_edit.py
+++ pitivi/ui/title_edit.py
@@ -10,6 +10,12 @@ def get_color(c):
         c.props.color.blue_float,
         c.props.alpha / 65535.0)
 
+def set_color(c, t):
+    c.props.color.red_float = t[0]
+    c.props.color.red_float = t[1]
+    c.props.color.red_float = t[2]
+    c.props.alpha = int(t[3] * 65535.0)
+
 alignments = [
         (0.0, 0.0), (0.5, 0.0), (1.0, 0.0),
         (0.0, 0.5), (0.5, 0.5), (1.0, 0.5),
@@ -18,24 +24,36 @@ alignments = [
 class TitleEditDialog(GladeWindow):
     glade_file = "title_edit.glade"
 
-    def __init__(self):
+    def __init__(self, **kw):
         GladeWindow.__init__(self)
 
+        self.text = kw.get('text', 'Hello, World!')
+        self.font = kw.get('font', 'Sans')
+        self.text_size = kw.get('text_size', 64)
+        self.bg_color = kw.get('bg_color', (0, 0, 0, 1))
+        self.fg_color = kw.get('fg_color', (1, 1, 1, 1))
         # Centre alignment is the default.
-        self.widgets['radiobutton5'].props.active = True
-
-        buffer = self.widgets['textview'].props.buffer
-        buffer.set_text('Hello, World!')
-
-        self.text = None
-        self.font = None
-        self.text_size = None
-        self.bg_color = None
-        self.fg_color = None
+        self.x_alignment = 0.5
+        self.y_alignment = 0.5
 
         # Hack: GladeWindow hides TitleEditDialog's run() with gtk.Dialog's;
         # undo that.
         del self.run
+
+    def _copy_to_dialog(self):
+        buffer = self.widgets['textview'].props.buffer
+        buffer.set_text(self.text)
+
+        set_color(self.widgets['bgcolor_button'], self.bg_color)
+        set_color(self.widgets['fgcolor_button'], self.fg_color)
+
+        self.widgets['fontbutton'].props.font_name = \
+            '%s %d' % (self.font, self.text_size)
+
+        for i, (x_alignment, y_alignment) in enumerate(alignments):
+            if (self.x_alignment == x_alignment and
+                self.y_alignment == y_alignment):
+                self.widgets['radiobutton%d' % (i + 1)].props.active = True
 
     def _copy_from_dialog(self):
         buffer = self.widgets['textview'].props.buffer
@@ -56,6 +74,7 @@ class TitleEditDialog(GladeWindow):
         self.y_alignment = y_alignment
 
     def run(self):
+        self._copy_to_dialog()
         response = gtk.Dialog.run(self.window)
         self._copy_from_dialog()
         return response
