914f3078ffa224111886cd7de8239b3ec67c72b6
Dafydd Harries <daf@rhydd.org>
2010-03-27 23:05:49 -0400

    copy title text to source list icon title after editing

--- pitivi/ui/sourcelist.py
+++ pitivi/ui/sourcelist.py
@@ -867,6 +867,7 @@ class SourceList(gtk.VBox, Loggable):
                     props[key] = getattr(dialog, key)
 
                 factory.set(**props)
+                self.storemodel[path][COL_SHORT_TEXT] = dialog.text
 
     def _textBoxButtonPressEventCb(self, textbox, event):
         if event.button == 3:
