e2ef55d1ac6d34b017d4bd75db9e2f4b420a22c1
Dafydd Harries <daf@rhydd.org>
2010-03-28 20:54:50 -0400

    title factory: override setFilterCaps so width/height get set on src

--- pitivi/factories/title.py
+++ pitivi/factories/title.py
@@ -21,26 +21,47 @@ class TitleSourceFactory(SourceFactory):
         self.addOutputStream(VideoStream(caps))
         self.source_kw = kw
         self.default_duration = 5 * gst.SECOND
+        self.filter_caps = None
 
     def _makeDefaultBin(self):
         return self._makeStreamBin(self.output_streams[0])
 
     def _makeStreamBin(self, output_stream=None):
+        # The project should have told us what its caps are.
+        assert self.filter_caps
+
         bin = gst.Bin()
-        src = TitleSource(**self.source_kw)
+        bin.src = TitleSource(**self.source_kw)
+        pad = bin.src.get_pad('src')
+        pad.set_caps(self.filter_caps)
+
         freeze = ImageFreeze()
         csp = gst.element_factory_make('ffmpegcolorspace')
         capsfilter = gst.element_factory_make('capsfilter')
         capsfilter.props.caps = output_stream.caps.copy()
 
-        bin.add(src, freeze, csp, capsfilter)
-        gst.element_link_many(src, freeze, csp, capsfilter)
+        bin.add(bin.src, freeze, csp, capsfilter)
+        gst.element_link_many(bin.src, freeze, csp, capsfilter)
 
         target = capsfilter.get_pad('src')
         ghost = gst.GhostPad('src', target)
         bin.add_pad(ghost)
 
         return bin
+
+    def setFilterCaps(self, caps):
+        assert not caps.is_empty()
+        assert caps[0].has_field('width')
+        assert caps[0].has_field('height')
+        # All we really care about is getting the resolution right; other
+        # elements will take care of the other aspects.
+        self.filter_caps = gst.Caps('video/x-raw-rgb,depth=32,bpp=32')
+        self.filter_caps[0]['width'] = caps[0]['width']
+        self.filter_caps[0]['height'] = caps[0]['height']
+
+        for bin in self.bins:
+            pad = bin.src.get_pad('src')
+            pad.set_caps(self.filter_caps)
 
     def _releaseBin(self, bin):
         pass
